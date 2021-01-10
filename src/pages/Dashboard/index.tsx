0161586de3c9e60f7bd8c35126d27c205a1e1d66
Crummorn <Igorcaze@gmail.com>
2021-01-10 11:28:23 -0300

    Feat: Adicionado conexão com a api do github para pesquisar repositorios existentes.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,77 +1,70 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github.</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/30866274?s=460&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
-            alt="Igor Cazé"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>IgorCazeNunes/Github_Explorer</strong>
-            <p>👨‍💻 Sistema para visualização de dados do Github.</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/30866274?s=460&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
-            alt="Igor Cazé"
-          />
-
-          <div>
-            <strong>IgorCazeNunes/Github_Explorer</strong>
-            <p>👨‍💻 Sistema para visualização de dados do Github.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/30866274?s=460&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
-            alt="Igor Cazé"
-          />
-
-          <div>
-            <strong>IgorCazeNunes/Github_Explorer</strong>
-            <p>👨‍💻 Sistema para visualização de dados do Github.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/30866274?s=460&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
-            alt="Igor Cazé"
-          />
-
-          <div>
-            <strong>IgorCazeNunes/Github_Explorer</strong>
-            <p>👨‍💻 Sistema para visualização de dados do Github.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
