119f368076e79ee933e0be3638b279de5421e667
Crummorn <Igorcaze@gmail.com>
2021-01-10 12:44:42 -0300

    Chore: Adicionado estrutura e estilização da pagina Repository.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -9,10 +15,61 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/30866274?s=460&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
+            alt="Igor Cazé"
+          />
+
+          <div>
+            <strong>IgorCazeNunes/GoFinances</strong>
+            <p>Descrição?</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>123132</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>123132</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>123132</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link
+          // key={repository.full_name}
+          to="/repository/repository.full_name"
+        >
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    color: #a8a8b3;
+    text-decoration: none;
+
+    transition: color 0.2s;
+
+    &:hover {
+      color: #6666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+
+    margin-top: 40px;
+
+    list-style: none;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+
+        color: #3d3d4d;
+        font-size: 36px;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #ffffff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin: 0 16px;
+
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
